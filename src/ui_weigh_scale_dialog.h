8ec04d292a7c62a8c4cad4e140a614c65b71def1
Anthony Hoare <hoarea@mcmaster.ca>
2026-01-20 13:41:51 -0500

    increased table size on weight scale dialog to show more measurements

--- src/ui_weigh_scale_dialog.h
+++ src/ui_weigh_scale_dialog.h
@@ -15,6 +15,7 @@
 #include <QtWidgets/QGroupBox>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QVBoxLayout>
 #include "widgets/measurement_table.h"
 #include "widgets/serial_port_widget.h"
@@ -32,12 +33,13 @@ public:
     QHBoxLayout *horizontalLayout;
     QPushButton *zeroDevicePushButton;
     MeasurementTable *measurementTable;
+    QSpacerItem *verticalSpacer;
 
     void setupUi(QDialog *WeighScaleDialog)
     {
         if (WeighScaleDialog->objectName().isEmpty())
             WeighScaleDialog->setObjectName(QString::fromUtf8("WeighScaleDialog"));
-        WeighScaleDialog->resize(654, 133);
+        WeighScaleDialog->resize(559, 514);
         verticalLayout_2 = new QVBoxLayout(WeighScaleDialog);
         verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
         testInfoWidget = new TestInfoWidget(WeighScaleDialog);
@@ -65,8 +67,17 @@ public:
 
         measurementTable = new MeasurementTable(WeighScaleDialog);
         measurementTable->setObjectName(QString::fromUtf8("measurementTable"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Maximum);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(measurementTable->sizePolicy().hasHeightForWidth());
+        measurementTable->setSizePolicy(sizePolicy);
 
         verticalLayout_2->addWidget(measurementTable);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_2->addItem(verticalSpacer);
 
 
         retranslateUi(WeighScaleDialog);
