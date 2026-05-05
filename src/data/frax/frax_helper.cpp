40345722aca9930c6af34c6854977b31d275edfd
Anthony Hoare <hoarea@mcmaster.ca>
2026-05-05 11:00:32 -0400

    frax fixes

--- src/data/frax/frax_helper.cpp
+++ src/data/frax/frax_helper.cpp
@@ -16,20 +16,14 @@ void FraxHelper::calculateAdditionalVariables(QJsonObject &input)
     const bool father_hip_fracture = input.value("father_hip_fracture").toBool();
     const bool mother_hip_fracture = input.value("mother_hip_fracture").toBool();
 
-    //// Calculate new variables
+    // Derived
     const bool parent_hip_fracture = father_hip_fracture || mother_hip_fracture;
+
     const int glucocorticoid_number = input.value("glucocorticoid_number").toInt();
     const int glucocorticoid_year = input.value("glucocorticoid_year").toInt();
     const int glucocorticoid_age = input.value("glucocorticoid_age").toInt();
 
-    const QString ra_medications = input.value("ra_medications").toString();
-
-    const double t_score = FraxHelper::calculateTScore(femoral_neck_bmd);
-
-    const double bmi = FraxHelper::calculateBmi(weight, height);
-
-    const bool rheumatoid_arthritis = ra_medications != "None" && !ra_medications.isEmpty();
-
+    // Derived
     const bool glucocorticoid = FraxHelper::calculateGlucocorticoid(
         age,
         glucocorticoid_number,
@@ -37,14 +31,23 @@ void FraxHelper::calculateAdditionalVariables(QJsonObject &input)
         glucocorticoid_age
     );
 
+    const QString ra_medications = input.value("ra_medications").toString();
+
+    // Derived
+    const bool rheumatoid_arthritis = ra_medications.toLower() != "none" && !ra_medications.isEmpty();
+
+    const double t_score = FraxHelper::calculateTScore(femoral_neck_bmd);
+    const double bmi = FraxHelper::calculateBmi(weight, height);
+
+    input.insert("type",                     "t");
+    input.insert("country_code",             "19");
     input.insert("rheumatoid_arthritis",     rheumatoid_arthritis);
     input.insert("secondary_osteoporosis",   false);
     input.insert("glucocorticoid",           glucocorticoid);
     input.insert("parent_hip_fracture",      parent_hip_fracture);
     input.insert("femoral_neck_tscore",      t_score);
     input.insert("body_mass_index",          bmi);
-    input.insert("type",                     "t");
-    input.insert("country_code",             "19");
+
 
     Utilities::prettyPrint(input);
 }
@@ -58,21 +61,19 @@ double FraxHelper::calculateBmi(const double weightKg, const double heightCm)
     return weightKg / (height_m * height_m);
 }
 
-double FraxHelper::calculateTScore(const double femoralNeckBmd)
+double FraxHelper::calculateTScore(const double bmd)
 {
-    const double M_value = 0.849;
-    const double sigma = 0.111;
-    const double bmd = femoralNeckBmd;
-    const double t_score = ((bmd / M_value) - 1) * (M_value / sigma);
-
-    return t_score;
+    double M = 0.849f;     // median age matched reference value
+    double L = 1.0f;       // skewness of the LMS reference curve corresponding to age peak BMD
+    double sigma = 0.111f; // age matched population standard deviation
+    return ((M * ((bmd / M ) - 1.0f))/(L * sigma));
 }
 
 double FraxHelper::calculateGlucocorticoid(
-    const int age,
-    const int glucocorticoid_number,
-    const int glucocorticoid_year,
-    const int glucocorticoid_age)
+    const int age,                    // age in years
+    const int glucocorticoid_number,  // how many months over your entire life did you take them
+    const int glucocorticoid_year,    // at what year did you last take them
+    const int glucocorticoid_age)     // at what age (yrs) did you last use them
 {
     int res = 0;
 
