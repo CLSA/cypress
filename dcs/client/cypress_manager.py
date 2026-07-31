46b94641c3b65ff5949d2e745c5e0aa7962b8a0a
Anthony Hoare <hoarea@mcmaster.ca>
2026-07-31 10:32:26 -0400

    cypress manager app improvements and simplification

--- dcs/client/cypress_manager.py
+++ dcs/client/cypress_manager.py
@@ -1,6 +1,8 @@
 import sys
-import requests
-import urllib3
+import os
+import subprocess
+import datetime
+import win32api
 
 from typing import override
 
@@ -12,6 +14,8 @@ from ui.ui_main_window import Ui_MainWindow
 
 from config import CypressConfig
 
+from utils import stop_process, is_process_running
+
 cypress_config, errors = CypressConfig.from_ini()
 if errors:
     print(errors)
@@ -20,27 +24,53 @@ if errors:
 host = cypress_config.host
 port = cypress_config.port
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-#urllib3.disable_warnings(urllib3.exceptions.)
-
 class CypressAPI:
     @staticmethod
-    def get_status():
-        response = requests.get(f"https://{host}:{port}/status", verify=False, timeout=1)
-        response.raise_for_status()
-        return response.json()
+    def start():
+        if is_process_running(cypress_config.name):
+            return True
+
+        if not cypress_config.exe.exists():
+            return False
+
+        if not cypress_config.exe.is_file():
+            return False
+
+        subprocess.Popen([str(cypress_config.exe.resolve())])
+
+        return True
 
     @staticmethod
-    def restart():
-        response = requests.post(f"https://{host}:{port}/restart", verify=False, timeout=1)
-        response.raise_for_status()
-        return response.text.strip('"')
+    def stop():
+        return stop_process(cypress_config.name)
 
     @staticmethod
-    def launch_device_detached(device: str):
-        response = requests.post(f"https://{host}:{port}/detached/{device}", verify=False, timeout=1)
-        response.raise_for_status()
-        return response.text.strip('"')
+    def last_modified():
+        exe_path = cypress_config.exe
+        mod_time = os.stat(exe_path).st_mtime
+        last_modified = datetime.datetime.fromtimestamp(mod_time)
+        return last_modified.strftime("%Y-%m-%d %H:%M:%S")
+
+    @staticmethod
+    def get_version():
+        # https://stackoverflow.com/questions/68774795/grabbing-full-file-version-of-an-exe-in-python
+        try:
+            info = win32api.GetFileVersionInfo(str(cypress_config.exe.resolve()), "\\")
+        except:
+            return
+
+        ms = info["FileVersionMS"]
+        ls = info["FileVersionLS"]
+
+        version = [
+            str(win32api.HIWORD(ms)),
+            str(win32api.LOWORD(ms)),
+            str(win32api.HIWORD(ls)),
+            str(win32api.LOWORD(ls)),
+        ]
+
+        return ".".join(version)
+
 
 class CypressManagerWindow(QMainWindow):
     def __init__(self):
@@ -48,6 +78,9 @@ class CypressManagerWindow(QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.setWindowTitle("Cypress manager")
+        self.setWindowIcon(QIcon("favicon.ico"))
 
         self.tray_icon = QSystemTrayIcon(self)
         self.tray_icon.setIcon(QIcon("favicon.ico"))
@@ -68,31 +101,14 @@ class CypressManagerWindow(QMainWindow):
         self.tray_icon.setContextMenu(tray_menu)
         self.tray_icon.show()
 
-        self.ui.restartCypress.clicked.connect(self.restart_cypress)
-
-        self.launch_device_btns = [
-            ("hearcon", self.ui.launchAudiometer),
-            ("cdtt", self.ui.launchCDTT),
-            ("choice_reaction_test", self.ui.launchCRT),
-            ("frax", self.ui.launchFRAX),
-            ("dxa", self.ui.launchDEXA),
-            ("mac5", self.ui.launchECG),
-            ("hand_grip", self.ui.launchGripStrength),
-            ("tonometer", self.ui.launchTonometer),
-            ("spirometer", self.ui.launchSpirometer),
-            ("weigh_scale", self.ui.launchWeighScale),
-            ("echo", self.ui.launchECHO),
-            ("general_proxy", self.ui.launchGeneralProxy)
-        ]
-
-        for device_name, btn in self.launch_device_btns:
-            btn.clicked.connect(lambda checked, name=device_name: self.launch_device(name))
+        self.ui.stopCypress.clicked.connect(self.stop_cypress)
+        self.ui.startCypress.clicked.connect(self.start_cypress)
 
         self.get_status()
 
         self.status_timer = QTimer(self)
         self.status_timer.timeout.connect(self.get_status)
-        #self.status_timer.start() # ms
+        self.status_timer.start(1000)  # ms
 
     @override
     def show(self):
@@ -105,75 +121,41 @@ class CypressManagerWindow(QMainWindow):
         elif QSystemTrayIcon.ActivationReason.Trigger == reason:
             self.show()
 
-    def launch_device(self, device_name):
-        self.enable_device_buttons(False)
+    def start_cypress(self):
+        self.ui.startCypress.setEnabled(False)
         QApplication.processEvents()
+        CypressAPI.start()
 
-        try:
-            CypressAPI.launch_device_detached(device_name)
-        except requests.HTTPError as e:
-            print(e)
-        except (requests.ConnectionError, requests.Timeout):
-            self._set_connection_error()
-        except Exception as e:
-            print(e)
-        finally:
-            self.enable_device_buttons(True)
-
-
-    def restart_cypress(self):
-        try:
-            CypressAPI.restart()
-        except requests.HTTPError as e:
-            print(e)
-        except (requests.ConnectionError, requests.Timeout):
-            self._set_connection_error()
-        except Exception as e:
-            print(e)
+    def stop_cypress(self):
+        CypressAPI.stop()
 
     def get_status(self):
-        try:
-            self.status = CypressAPI.get_status()
-            version = self.status.get("version", "Unknown")
+        version = CypressAPI.get_version()
+        last_modified = CypressAPI.last_modified()
+        host = cypress_config.host
 
-            if "session" in self.status:
-                self.ui.statusValue.setText(f"device in progress")
-                self.enable_device_buttons(False)
-            else:
-                self.ui.statusValue.setText("running")
-                self.enable_device_buttons(True)
+        self.ui.versionValue.setText(version if version else "---")
+        self.ui.lastUpdatedValue.setText(last_modified)
+        self.ui.hostValue.setText(host)
 
-            self.ui.versionValue.setText(version)
-            self.ui.hostValue.setText(f"{cypress_config.host}")
-            self.ui.lastUpdatedValue.setText(f"{self.status["last_updated"]}")
-            #self.ui.statusValue.setText(json.dumps(self.status, indent=4))
-        except requests.HTTPError as e:
-            print(e)
-        except (requests.ConnectionError, requests.Timeout):
-            self._set_connection_error()
-        except Exception as e:
-            print(e)
-
-    def enable_device_buttons(self, enabled: bool):
-        for device_name, btn in self.launch_device_btns:
-            btn.setEnabled(enabled if device_name in self.status["installed"] else False)
+        if is_process_running(cypress_config.name):
+            self.ui.statusValue.setText("running")
+            self.ui.startCypress.setEnabled(False)
+            self.ui.stopCypress.setEnabled(True)
+        else:
+            self.ui.statusValue.setText("not running")
+            self.ui.startCypress.setEnabled(True)
+            self.ui.stopCypress.setEnabled(False)
 
     def closeEvent(self, event):
         event.accept()  # Closes the window
 
-    def _set_connection_error(self):
-        self.ui.statusValue.setText("Could not connect to Cypress")
-        self.ui.versionValue.setText("---")
-        self.ui.hostValue.setText("---")
-        self.ui.lastUpdatedValue.setText("---")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication()
     app.setQuitOnLastWindowClosed(False)
 
     main_window = CypressManagerWindow()
-    main_window.show()
+    main_window.hide()
 
     sys.exit(app.exec())
-
-
