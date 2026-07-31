e0aa8aa0e20e79375b0afe4337074da2ca332aae
Anthony Hoare <hoarea@mcmaster.ca>
2026-07-31 10:29:23 -0400

    more cypress config options, easier loading of test configs

--- dcs/client/config.py
+++ dcs/client/config.py
@@ -15,21 +15,20 @@ def is_executable(path: Path):
 
 
 class BaseConfig(BaseModel):
-    config_name: ClassVar[str] = "config.ini"
     section_name: ClassVar[str] = ""
 
     @classmethod
-    def from_ini(cls):
+    def from_ini(cls, path: Path ="config.ini"):
         try:
             config = configparser.ConfigParser()
 
-            if not Path(cls.config_name).exists():
-                raise FileNotFoundError(f"Config file not found: {cls.config_name}")
+            if not Path(path).exists():
+                raise FileNotFoundError(f"Config file not found: {str(path.resolve())}")
 
-            config.read(cls.config_name)
+            config.read(path)
 
             if cls.section_name not in config:
-                raise ValueError(f"Section {cls.section_name} not found in {cls.config_name}")
+                raise ValueError(f"Section {cls.section_name} not found in {str(path.resolve())}")
 
             settings = config[cls.section_name]
             raw_settings = dict(settings.items())
@@ -46,10 +45,10 @@ class CypressConfig(BaseConfig):
     # Section in .ini
     section_name: ClassVar[str] = "cypress"
 
-    # HTTP server host
+    # Client HTTP server host
     host: str
 
-    # HTTP server port
+    # Client HTTP server port
     port: Annotated[int, PositiveInt]
 
     # Comma delimited list of hosts to allow requests from
@@ -60,8 +59,12 @@ class CypressConfig(BaseConfig):
     # Pine authentication token
     auth: str
 
+    # Client SSL
     ssl_certfile: FilePath | None
     ssl_keyfile: FilePath | None
+
+    name: str
+    exe: FilePath
 
 
 class DeviceConfig(BaseConfig):
